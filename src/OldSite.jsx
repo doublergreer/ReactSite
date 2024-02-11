eeae2ea1858f70ff72b23512f3923d70b3dab1cc
Ryan Greer <ryanrgreer@outlook.com>
2024-02-10 21:57:24 -0700

    added delete for whole page

--- src/OldSite.jsx
+++ src/OldSite.jsx
@@ -1,17 +1,58 @@
+import React, { useState } from 'react';
+
 export default function OldSite() {
+    const [isHovered, setIsHovered] = useState(false);
+    const [buttonClicked, setButtonClicked] = useState(0);
+
+    // Function to handle mouse over event
+    const handleMouseOver = () => {
+        setIsHovered(true);
+    };
+    
+    // Function to handle mouse out event
+    const handleMouseOut = () => {
+        setIsHovered(false);
+    };
+
+    const handleClick = () => {
+        setButtonClicked(buttonClicked+1);
+    };
+
     return (
+        <>
         <header> 
-                <p id="title">Ryan Greer's Website</p>
-                <div class="projects-title">
-                    <h2>Links:</h2>
+                {buttonClicked > 1 || <p id="title">Ryan Greer's Website</p>}
+                {buttonClicked >= 1 && buttonClicked < 5 && <button onClick={handleClick} width="20" height="20" >Delete</button>} 
+                {/* {(isHovered || buttonClicked >= 1) && buttonClicked < 5 && <button onClick={handleClick} width="20" height="20" >Delete</button>}  */}
+                <div className="projects-title">
+                    {buttonClicked > 2 || <h2 id="links">Links:</h2>}
                 </div>
-                <div className="projects">
-                    <a href="https://github.com/doublergreer" target="_blank"> Github </a>
+                {buttonClicked > 3 || <div className="projects">
+                    <a href="https://github.com/doublergreer" target="_blank" id="github"> Github </a>
                     <br/>
                     <br/>
-                    <a href="https://www.linkedin.com/in/ryan-greer-b1123424a/" target="_blank"> Linkedin </a>
+                    <a href="https://www.linkedin.com/in/ryan-greer-b1123424a/" target="_blank" id="github"> Linkedin </a>
                     <br/> <br/>
-                </div>
+                </div>}
+
+                {buttonClicked >= 5 && <div>
+                    <h1>Well done...</h1>
+                    <h3>It's All Gone</h3>
+                </div>}
         </header>
+        <DestructSite isHovered={isHovered} buttonClicked={buttonClicked} handleMouseOver={handleMouseOver} handleMouseOut={handleMouseOut} handleClick={handleClick} />
+        </>
+    );
+}
+
+function DestructSite(props) {
+
+    return (
+        <>
+            {props.buttonClicked > 0 || <a onMouseOver={props.handleMouseOver} onMouseOut={props.handleMouseOut} className='destroy'>
+                Destroy Website
+                    {props.isHovered && <button onClick={props.handleClick} width="20" height="20" >Delete</button>}
+            </a> }
+        </>
     )
 }
